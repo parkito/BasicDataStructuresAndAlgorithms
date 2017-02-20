ca4596fb49a3101d7624defc103fbbfacd29d457
parkito <artyom-karnov@yandex.ru>
2017-02-20 22:06:58 +0300

    Global refactoring

---
+++ src/test/java/com/saiu/dataStructures/dynamicArray/DynamicArrayTest.java
@@ -0,0 +1,35 @@
+package com.saiu.dataStructures.dynamicArray;
+
+import org.junit.Test;
+
+/**
+ * @author Artem Karnov @date 29.11.2016.
+ *         artem.karnov@t-systems.com
+ **/
+public class DynamicArrayTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void size() throws Exception {
+
+    }
+
+    @Test
+    public void contains() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/saiu/dataStructures/simpleLinkedList/LinkedListTest.java
@@ -0,0 +1,55 @@
+package com.saiu.dataStructures.simpleLinkedList;
+
+import org.junit.Test;
+
+/**
+ * @author Artem Karnov @date 29.11.2016.
+ *         artem.karnov@t-systems.com
+ **/
+public class LinkedListTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void displayAll() throws Exception {
+
+    }
+
+    @Test
+    public void removeFirst() throws Exception {
+
+    }
+
+    @Test
+    public void removeLast() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void size() throws Exception {
+
+    }
+
+    @Test
+    public void contains() throws Exception {
+
+    }
+
+}
