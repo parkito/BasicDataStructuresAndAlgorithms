2c16d1da620499487f4e8d77819e2b4ed68f943b
parkito <artyom-karnov@yandex.ru>
2017-05-03 16:17:42 +0300

    Added Cartesian product stream algorithm

--- src/main/java/com/saiu/language/labmdas/examples/streams_dogs/CreateSourcesFromDataStructures.java
+++ src/main/java/com/saiu/language/labmdas/examples/streams_dogs/CreateSourcesFromDataStructures.java
@@ -3,7 +3,9 @@ package com.saiu.language.labmdas.examples.streams_dogs;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -26,5 +28,39 @@ public class CreateSourcesFromDataStructures {
         return IntStream
                 .range(0, list.size())
                 .mapToObj(idx -> new IndexExampleClass<>(idx, list.get(idx)));
+    }
+
+    //Cartesian product of 3 lists
+    public static Stream<String> limitedCartesianProduct() {
+        List<List<String>> input = Arrays.asList(
+                Arrays.asList("a", "b", "c"),
+                Arrays.asList("x", "y"),
+                Arrays.asList("1", "2", "3")
+        );
+
+        return input.get(0).stream().flatMap(a ->
+                input.get(1).stream().flatMap(b ->
+                        input.get(2).stream()
+                                .map(c -> a + b + c)));
+    }
+
+    //Cartesian product of n lists
+    public static Stream<String> unlimitedCartesianProduct() {
+        List<List<String>> input = Arrays.asList(
+                Arrays.asList("a", "b", "c"),
+                Arrays.asList("x", "y"),
+                Arrays.asList("1", "2", "3")
+        );
+
+        Supplier<Stream<String>> s = input.stream()
+                //Stream<List<String>>
+                .<Supplier<Stream<String>>>map(list -> list::stream)
+                //Stream<Supplier<Stream<String>>>
+                .reduce((sup1, sup2) -> () -> sup1.get()
+                        .flatMap(e1 -> sup2.get().map(e2 -> e1 + e2)))
+                //Optional<Supplier<Stream<String>>>
+                .orElse(() -> Stream.of(""));
+
+        return s.get();
     }
 }
