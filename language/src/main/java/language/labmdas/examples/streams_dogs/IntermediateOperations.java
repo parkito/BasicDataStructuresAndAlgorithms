4377669ce3aa966492d9319105e42af48c565785
parkito <artyom-karnov@yandex.ru>
2018-01-03 17:42:08 +0300

    Fix lambdas tests

--- language/src/main/java/language/labmdas/examples/streams_dogs/IntermediateOperations.java
+++
@@ -1,9 +0,0 @@
-package language.labmdas.examples.streams_dogs;
-
-/**
- * @author Artem Karnov @date 03.05.2017.
- *         artem.karnov@t-systems.com
- */
-
-public class IntermediateOperations {
-}
