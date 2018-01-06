0bb2afb3d5dcd0c19e68c92f742b2807153a8526
parkito <artyom-karnov@yandex.ru>
2018-01-06 16:49:26 +0300

    Improve arithmetic parser

---
+++ algorithm/src/main/java/ru/siksmfp/basic/algorithm/math/Math.java
@@ -0,0 +1,13 @@
+package ru.siksmfp.basic.algorithm.math;
+
+/**
+ * @author Artem Karnov @date 1/6/2018.
+ * artem.karnov@t-systems.com
+ */
+public class Math {
+    public static double pow(double base, double exponent) {
+        // TODO: 1/6/2018  make your own realization
+        return java.lang.Math.pow(base, exponent);
+    }
+
+}
--- algorithm/src/main/java/ru/siksmfp/basic/algorithm/parse/ArithmeticParser.java
+++ algorithm/src/main/java/ru/siksmfp/basic/algorithm/parse/ArithmeticParser.java
@@ -1,6 +1,11 @@
 package ru.siksmfp.basic.algorithm.parse;
 
+import ru.siksmfp.basic.algorithm.math.Math;
+import ru.siksmfp.basic.structure.array.dynamic.Array;
 import ru.siksmfp.basic.structure.stack.Stack;
+
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 /**
  * @author Artem Karnov @date 1/5/2018.
@@ -10,24 +15,41 @@ public class ArithmeticParser {
     private static final char OPENED_PARENTHESES = '(';
     private static final char CLOSED_PARENTHESES = ')';
     private static final char SPACE = ' ';
-    private static final char CARET = '^';
+    private static final Pattern DOUBLE_PATTERN = Pattern.compile(
+            "[\\x00-\\x20]*[+-]?(NaN|Infinity|((((\\p{Digit}+)(\\.)?((\\p{Digit}+)?)" +
+                    "([eE][+-]?(\\p{Digit}+))?)|(\\.((\\p{Digit}+))([eE][+-]?(\\p{Digit}+))?)|" +
+                    "(((0[xX](\\p{XDigit}+)(\\.)?)|(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+)))" +
+                    "[pP][+-]?(\\p{Digit}+)))[fFdD]?))[\\x00-\\x20]*");
 
     public static void main(String[] args) {
         ArithmeticParser parser = new ArithmeticParser();
 
-        System.out.println(parser.infixToPostfix("A+B+C")); //A B C + +
-        System.out.println(parser.infixToPostfix("A+B*C")); //A B C * +
-        System.out.println(parser.infixToPostfix("A*(B+C)")); //A B C + *
-        System.out.println(parser.infixToPostfix("A + B * (C - D)")); //A B C D - * +
-        System.out.println(parser.infixToPostfix("A*(B+C)-D/(E+F)")); //A B C + * D E F + / -
-        System.out.println(parser.infixToPostfix("A*(B+C)*D")); //A B C + * D *
-        System.out.println(parser.infixToPostfix("A^B*C")); //A B ^ C *
-        System.out.println(parser.infixToPostfix("(A+B)^C+D")); //A B + C ^ D +
+//        System.out.println(parser.infixToPostfix("A+B+C")); //A B C + +
+//        System.out.println(parser.infixToPostfix("A+B*C")); //A B C * +
+//        System.out.println(parser.infixToPostfix("A*(B+C)")); //A B C + *
+//        System.out.println(parser.infixToPostfix("A + B * (C - D)")); //A B C D - * +
+//        System.out.println(parser.infixToPostfix("A*(B+C)-D/(E+F)")); //A B C + * D E F + / -
+//        System.out.println(parser.infixToPostfix("A*(B+C)*D")); //A B C + * D *
+//        System.out.println(parser.infixToPostfix("A^B*C")); //A B ^ C *
+//        System.out.println(parser.infixToPostfix("(A+B)^C+D")); //A B + C ^ D +
+
+//        System.out.println(parser.calculateExpression("1 2 3 + +")); //6
+//        System.out.println(parser.calculateExpression("1 2 3 * +")); //7
+//        System.out.println(parser.calculateExpression("1 2 3 4 - * +")); //-1
+//        System.out.println(parser.calculateExpression("2 2 3.1 + * 4 1 1 + / -")); //8.2
+//        System.out.println(parser.calculateExpression("2 2 ^")); //4
+//        System.out.println(parser.calculateExpression("2 2 2 ^ ^")); //16
+
+        System.out.println(parser.parse("1+1")); //1
+        System.out.println(parser.parse("1+1+1")); //3
+        System.out.println(parser.parse("1+(1+1)")); //3
+        System.out.println(parser.parse("1^(1+1)")); //1 isn't cor
 
     }
-//    public double parse(String arithmeticString) {
-//
-//    }
+    public double parse(String arithmeticString) {
+        String postfix = infixToPostfix(arithmeticString);
+        return calculateExpression(postfix);
+    }
 
     private String infixToPostfix(String infixString) {
         String infixStringWithoutSpaces = infixString.replaceAll("\\s+", "");
@@ -67,9 +89,37 @@ public class ArithmeticParser {
         return stringBuilder.toString();
     }
 
-//    private double calculateExpression(String postfix) {
-//
-//    }
+    private double calculateExpression(String postfix) {
+        Double result = null;
+        Array<String> expressionArray = new Array();
+        StringTokenizer tokenizer = new StringTokenizer(postfix, " ");
+        while (tokenizer.hasMoreElements()) {
+            expressionArray.add(tokenizer.nextToken());
+        }
+
+        Stack<Character> operatorStacks = new Stack<>();
+        for (int i = expressionArray.size() - 1; i >= 0; i--) {
+            String currentExpression = expressionArray.get(i);
+            if (currentExpression.length() == 1 && getOperatorPriority(currentExpression.charAt(0)) > 0) {
+                operatorStacks.push(currentExpression.charAt(0));
+            } else {
+                while (!operatorStacks.isEmpty()) {
+                    if (result == null) {
+                        result = makeArithmeticOperation(Double.valueOf(expressionArray.get(i - 1)), Double.valueOf(expressionArray.get(i)), operatorStacks.pop());
+                        i--;
+                    } else {
+                        if (!isDouble(expressionArray.get(i))) {
+                            operatorStacks.push(expressionArray.get(i).charAt(0));
+                            break;
+                        }
+                        result = makeArithmeticOperation(result, Double.valueOf(expressionArray.get(i)), operatorStacks.pop());
+                    }
+                    i--;
+                }
+            }
+        }
+        return result;
+    }
 
     private void clearStack(StringBuilder stringBuilder, Stack<Character> operatorStack) {
         while (!operatorStack.isEmpty()) {
@@ -77,6 +127,23 @@ public class ArithmeticParser {
             if (currentOperator != OPENED_PARENTHESES) {
                 stringBuilder.append(currentOperator).append(SPACE);
             }
+        }
+    }
+
+    private double makeArithmeticOperation(double firstValue, double secondValue, char operator) {
+        switch (operator) {
+            case '^':
+                return Math.pow(firstValue, secondValue);
+            case '*':
+                return firstValue * secondValue;
+            case '/':
+                return secondValue / firstValue;
+            case '+':
+                return firstValue + secondValue;
+            case '-':
+                return firstValue - secondValue;
+            default:
+                throw new ArithmeticException("Unknown operator");
         }
     }
 
@@ -96,5 +163,9 @@ public class ArithmeticParser {
             default:
                 return 0;
         }
+    }
+
+    public static boolean isDouble(String s) {
+        return DOUBLE_PATTERN.matcher(s).matches();
     }
 }
