07f215836c7158f2bf024704821cfb39722f98c6
parkito <artyom-karnov@yandex.ru>
2018-01-30 21:36:06 +0300

    Added documentation for hash tables

--- structure/src/main/java/ru/siksmfp/basic/structure/hash/table/open/adress/linear/prob/LinearProbHashTable.java
+++ structure/src/main/java/ru/siksmfp/basic/structure/hash/table/open/adress/linear/prob/LinearProbHashTable.java
@@ -12,8 +12,16 @@ import java.util.function.Function;
 /**
  * @author Artem Karnov @date 1/10/2018.
  * @email artem.karnov@t-systems.com
+ * <p>
+ * Hash table with linear probing
  */
 public class LinearProbHashTable<K, V> implements HashTable<K, V> {
+    /**
+     * Structure for storing key-value structure
+     *
+     * @param <K> - key of structure
+     * @param <V> - value of structure
+     */
     private class Node<K, V> {
         private K key;
         private V value;
@@ -38,6 +46,9 @@ public class LinearProbHashTable<K, V> implements HashTable<K, V> {
         }
     }
 
+    /**
+     * Default hash function is being applying whe custom function isn't given
+     */
     private final Function<K, Integer> DEFAULT_HASH_FUNCTION = t -> {
         int i = t.hashCode();
         i = (i + 0x7ed55d16) + (i << 12);
@@ -62,6 +73,12 @@ public class LinearProbHashTable<K, V> implements HashTable<K, V> {
         hashFunction = DEFAULT_HASH_FUNCTION;
     }
 
+    /**
+     * Add key - value structure to hash table
+     *
+     * @param key   key for adding
+     * @param value value for adding
+     */
     @Override
     public void add(K key, V value) {
         int index = applyHashing(key);
@@ -85,11 +102,23 @@ public class LinearProbHashTable<K, V> implements HashTable<K, V> {
         }
     }
 
+    /**
+     * Key hashing with given hash function
+     *
+     * @param key key for hashing
+     * @return result of applying hash function on key
+     */
     @Override
     public int getHash(K key) {
         return hashFunction.apply(key);
     }
 
+    /**
+     * Getting value by key from hash table
+     *
+     * @param key key for getting
+     * @return stored value found by key
+     */
     @Override
     public V get(K key) {
         int index = applyHashing(key);
@@ -110,11 +139,25 @@ public class LinearProbHashTable<K, V> implements HashTable<K, V> {
         }
     }
 
+    /**
+     * Setting hash function for hash calculation
+     * <p>
+     * Be careful setting function after using initialized object of
+     * hash table. You can loose your data.
+     * Typical use case is set function before hash table using
+     *
+     * @param hashFunction hash function implementation
+     */
     @Override
     public void setHashFunction(Function<K, Integer> hashFunction) {
         this.hashFunction = hashFunction;
     }
 
+    /**
+     * Remove key - value structure by key
+     *
+     * @param key key for deleting
+     */
     @Override
     public void remove(K key) {
         int index = applyHashing(key);
@@ -137,6 +180,11 @@ public class LinearProbHashTable<K, V> implements HashTable<K, V> {
         }
     }
 
+    /**
+     * Replacing value of key - value structure on NULL by key
+     *
+     * @param key key for replacing
+     */
     @Override
     public void delete(K key) {
         int index = applyHashing(key);
@@ -157,6 +205,11 @@ public class LinearProbHashTable<K, V> implements HashTable<K, V> {
         }
     }
 
+    /**
+     * Getting number of adding elements
+     *
+     * @return number of stored elements of hash table
+     */
     @Override
     public int size() {
         return size;
@@ -218,6 +271,12 @@ public class LinearProbHashTable<K, V> implements HashTable<K, V> {
         return "LinearProbHashTable{" + result.toString() + "}";
     }
 
+    /**
+     * Calculating index of structure in allocated array
+     *
+     * @param key key for applying
+     * @return index of key in allocated array
+     */
     private int applyHashing(K key) {
         return hashFunction.apply(key) % array.size();
     }
