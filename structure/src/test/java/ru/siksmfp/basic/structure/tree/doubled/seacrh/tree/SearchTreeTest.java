2a2bc4e7c1335d987ea2a97d6040ccff4f8b00e7
parkito <artyom-karnov@yandex.ru>
2018-02-25 21:52:40 +0300

    Add tests for sbst

--- structure/src/test/java/ru/siksmfp/basic/structure/tree/doubled/seacrh/tree/SearchTreeTest.java
+++ structure/src/test/java/ru/siksmfp/basic/structure/tree/doubled/seacrh/tree/SearchTreeTest.java
@@ -1,6 +1,7 @@
 package ru.siksmfp.basic.structure.tree.doubled.seacrh.tree;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import ru.siksmfp.basic.structure.api.TreeStructure;
 
@@ -9,97 +10,156 @@ import ru.siksmfp.basic.structure.api.TreeStructure;
  * @email artem.karnov@t-systems.com
  */
 public class SearchTreeTest {
+    private static final int FIRST_KEY = 1;
+    private static final int SECOND_KEY = 2;
+    private static final int THIRD_KEY = 3;
+    private static final int FOURTH_KEY = 4;
+    private static final double INFELICITY = 0.001;
+    private static final String FIRST_VALUE = "1";
+    private static final String SECOND_VALUE = "2";
+    private static final String THIRD_VALUE = "3";
+    private static final String FOURTH_VALUE = "4";
+
+    class InnerMutableClass {
+        String stringField;
+        Double doubleField;
+
+        public InnerMutableClass(String stringField, Double doubleField) {
+            this.stringField = stringField;
+            this.doubleField = doubleField;
+        }
+
+        public String getStringField() {
+            return stringField;
+        }
+
+        public void setStringField(String stringField) {
+            this.stringField = stringField;
+        }
+
+        public Double getDoubleField() {
+            return doubleField;
+        }
+
+        public void setDoubleField(Double doubleField) {
+            this.doubleField = doubleField;
+        }
+
+        @Override
+        public String toString() {
+            return "InnerMutableClass{" +
+                    "stringField='" + stringField + '\'' +
+                    ", doubleField=" + doubleField +
+                    '}';
+        }
+    }
 
     @Test
     public void firstTest() {
         TreeStructure<Integer, String> tree = new SearchTree<>();
-        int firstKey = 1;
-        int secondKey = 2;
-        int thirdKey = 3;
 
-        String firstValue = "1";
-        String secondValue = "2";
-        String thirdValue = "3";
+        tree.add(FIRST_KEY, FIRST_VALUE);
+        tree.add(SECOND_KEY, SECOND_VALUE);
+        tree.add(THIRD_KEY, THIRD_VALUE);
 
-        tree.add(firstKey, firstValue);
-        tree.add(secondKey, secondValue);
-        tree.add(thirdKey, thirdValue);
-
-        Assert.assertNotNull(firstValue, tree.get(firstKey));
-        Assert.assertNotNull(secondValue, tree.get(secondKey));
-        Assert.assertNotNull(thirdValue, tree.get(thirdKey));
+        Assert.assertNotNull(FIRST_VALUE, tree.get(FIRST_KEY));
+        Assert.assertNotNull(SECOND_VALUE, tree.get(SECOND_KEY));
+        Assert.assertNotNull(THIRD_VALUE, tree.get(THIRD_KEY));
     }
 
     @Test
     public void secondTest() {
         TreeStructure<Integer, String> tree = new SearchTree<>();
-        int firstKey = 1;
-        int secondKey = 2;
-        int thirdKey = 3;
-        int fourthKey = 4;
 
-        String firstValue = "1";
-        String secondValue = "2";
-        String thirdValue = "3";
-
-        tree.add(firstKey, firstValue);
-        tree.add(secondKey, secondValue);
-        tree.add(thirdKey, thirdValue);
+        tree.add(FIRST_KEY, FIRST_VALUE);
+        tree.add(SECOND_KEY, SECOND_VALUE);
+        tree.add(THIRD_KEY, THIRD_VALUE);
 
         Assert.assertEquals(3, tree.size());
-        Assert.assertTrue(tree.contains(firstKey));
-        Assert.assertTrue(tree.contains(secondKey));
-        Assert.assertTrue(tree.contains(thirdKey));
-        Assert.assertFalse(tree.contains(fourthKey));
+        Assert.assertTrue(tree.contains(FIRST_KEY));
+        Assert.assertTrue(tree.contains(SECOND_KEY));
+        Assert.assertTrue(tree.contains(THIRD_KEY));
+        Assert.assertFalse(tree.contains(FOURTH_KEY));
     }
 
     @Test
     public void thirdTest() {
         TreeStructure<Integer, String> tree = new SearchTree<>();
-        int firstKey = 1;
-        int secondKey = 2;
-        int thirdKey = 3;
 
-        String firstValue = "1";
-        String secondValue = "2";
-        String thirdValue = "3";
-        String fourthValue = "4";
-
-        tree.add(firstKey, firstValue);
-        tree.add(secondKey, secondValue);
-        tree.add(thirdKey, thirdValue);
+        tree.add(FIRST_KEY, FIRST_VALUE);
+        tree.add(SECOND_KEY, SECOND_VALUE);
+        tree.add(THIRD_KEY, THIRD_VALUE);
 
         Assert.assertEquals(3, tree.size());
-        Assert.assertTrue(tree.containsValue(firstValue));
-        Assert.assertTrue(tree.containsValue(secondValue));
-        Assert.assertTrue(tree.containsValue(thirdValue));
-        Assert.assertFalse(tree.containsValue(fourthValue));
+        Assert.assertTrue(tree.containsValue(FIRST_VALUE));
+        Assert.assertTrue(tree.containsValue(SECOND_VALUE));
+        Assert.assertTrue(tree.containsValue(THIRD_VALUE));
+        Assert.assertFalse(tree.containsValue(FOURTH_VALUE));
     }
 
     @Test
     public void fourthTest() {
         TreeStructure<Integer, String> tree = new SearchTree<>();
-        int firstKey = 1;
-        int secondKey = 2;
-        int thirdKey = 3;
 
-        String firstValue = "1";
-        String secondValue = "2";
-        String thirdValue = "3";
-        String fourthValue = "4";
-
-        tree.add(firstKey, firstValue);
-        tree.add(secondKey, secondValue);
-        tree.add(thirdKey, thirdValue);
+        tree.add(FIRST_KEY, FIRST_VALUE);
+        tree.add(SECOND_KEY, SECOND_VALUE);
+        tree.add(THIRD_KEY, THIRD_VALUE);
 
         Assert.assertEquals(3, tree.size());
 
-        tree.remove(thirdKey);
+        tree.remove(THIRD_KEY);
 
         Assert.assertEquals(2, tree.size());
-        Assert.assertTrue(tree.containsValue(firstValue));
-        Assert.assertTrue(tree.containsValue(secondValue));
-        Assert.assertFalse(tree.containsValue(thirdValue));
-        Assert.assertFalse(tree.containsValue(fourthValue));
+        Assert.assertTrue(tree.containsValue(FIRST_VALUE));
+        Assert.assertTrue(tree.containsValue(SECOND_VALUE));
+        Assert.assertFalse(tree.containsValue(THIRD_VALUE));
+        Assert.assertFalse(tree.containsValue(FOURTH_VALUE));
+    }
+
+    @Test
+    public void fifthTest() {
+        TreeStructure<Integer, String> tree = new SearchTree<>();
+
+        tree.add(FIRST_KEY, FIRST_VALUE);
+        tree.add(SECOND_KEY, SECOND_VALUE);
+        tree.add(THIRD_KEY, THIRD_VALUE);
+
+        tree.removeValue(SECOND_VALUE);
+
+        Assert.assertEquals(2, tree.size());
+        Assert.assertTrue(tree.containsValue(FIRST_VALUE));
+        Assert.assertFalse(tree.containsValue(SECOND_VALUE));
+        Assert.assertTrue(tree.containsValue(THIRD_VALUE));
+    }
+
+    @Test
+    @Ignore
+    public void sixthTest() {
+        TreeStructure<Integer, InnerMutableClass> tree = new SearchTree<>();
+
+        String stringValue1 = "Value1";
+        String stringValue2 = "Value2";
+        String stringValue3 = "Value3";
+        Double doubleValue1 = 1.0;
+        Double doubleValue2 = 2.0;
+        Double doubleValue3 = 3.0;
+
+        InnerMutableClass innerMutableClass1 = new InnerMutableClass(stringValue1, doubleValue1);
+        InnerMutableClass innerMutableClass2 = new InnerMutableClass(stringValue2, doubleValue2);
+
+        tree.add(FIRST_KEY, innerMutableClass1);
+        tree.strictAdd(SECOND_KEY, innerMutableClass2);
+
+        innerMutableClass1.setStringField(stringValue3);
+        innerMutableClass1.setDoubleField(doubleValue3);
+
+        innerMutableClass2.setStringField(stringValue3);
+        innerMutableClass2.setDoubleField(doubleValue3);
+
+        Assert.assertEquals(2, tree.size());
+        Assert.assertEquals(stringValue3, tree.get(FIRST_KEY).getStringField());
+        Assert.assertEquals(doubleValue3, tree.get(FIRST_KEY).getDoubleField(), INFELICITY);
+        Assert.assertEquals(stringValue2, tree.get(SECOND_KEY).getStringField());
+        Assert.assertEquals(doubleValue2, tree.get(SECOND_KEY).getDoubleField(), INFELICITY);
     }
 }
