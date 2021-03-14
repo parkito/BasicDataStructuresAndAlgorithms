3823b3eb2c5a4c1e44a9179eb3498f263afbc43c
parkito <artyom-karnov@yandex.ru>
2021-03-14 15:05:24 -0600

    Improve special string

--- c/Source/22_special_string/SpecialString.cpp
+++ c/Source/22_special_string/SpecialString.cpp
@@ -12,7 +12,6 @@ long combinations(const int num) {
 
 bool is_middle(const std::string &s, const int &from, const int &middle) {
     char toCompare = s[from];
-//    std::cout << s.substr(from, (middle - from) * 2 + 1) << " ";
     for (int i = 1; i <= middle - from; ++i) {
         if (middle + i >= s.length() || s[middle - 1] != toCompare || s[middle - i] != s[middle + i]) {
             return false;
@@ -30,8 +29,12 @@ long substrCount(int n, std::string s) {
     for (int i = 1; i < s.length(); ++i) {
         if (s[i] != s[groupFirst]) {
             int elements = i - groupFirst;
+            while (!is_middle(s, groupFirst, i) && groupFirst != i) {
+                groupFirst++;
+            }
             if (is_middle(s, groupFirst, i)) {
-                counter += 1 + 2 * combinations(elements);
+                std::cout << s.substr(groupFirst, (i - groupFirst) * 2 + 1) << " ";
+                counter += 1 + 2 * combinations(i - groupFirst);
                 groupFirst = i;
             } else {
                 if (elements > 1) {
@@ -52,11 +55,12 @@ long substrCount(int n, std::string s) {
 
 int main() {
 //    std::cout << is_middle("aassa", 0, 2);
-    std::cout << substrCount(std::string("mnonopoo").size(), "mnonopoo") << std::endl; //12
-    std::cout << substrCount(std::string("asasd").size(), "asasd") << std::endl; //7
-    std::cout << substrCount(std::string("abcbaba").size(), "abcbaba") << std::endl; //10
-    std::cout << substrCount(std::string("aaaa").size(), "aaaa") << std::endl; //10
-    std::cout << substrCount(std::string("aaaa").size(),
-                             global::file_to_string("/code/Source/22_special_string/test.txt")) << std::endl; //1272919
+//    std::cout << substrCount(std::string("mnonopoo").size(), "mnonopoo") << std::endl; //12
+//    std::cout << substrCount(std::string("asasd").size(), "asasd") << std::endl; //7
+//    std::cout << substrCount(std::string("abcbaba").size(), "abcbaba") << std::endl; //10
+//    std::cout << substrCount(std::string("aaaa").size(), "aaaa") << std::endl; //10
+    std::cout << substrCount(std::string("aaaa").size(), "cccacc") << std::endl; //12
+//    std::cout << substrCount(std::string("aaaa").size(),
+//                             global::file_to_string("/code/Source/22_special_string/test.txt")) << std::endl; //1272919
 }
 
