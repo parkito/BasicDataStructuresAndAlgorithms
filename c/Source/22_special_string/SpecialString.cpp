8c399bd47233c841f6e8a3a9d8c906dfaa0a9df2
parkito <artyom-karnov@yandex.ru>
2021-03-14 15:30:22 -0600

    Complete special string

--- c/Source/22_special_string/SpecialString.cpp
+++ c/Source/22_special_string/SpecialString.cpp
@@ -10,14 +10,20 @@ long combinations(const int num) {
     return com;
 }
 
-bool is_middle(const std::string &s, const int &from, const int &middle) {
-    char toCompare = s[from];
-    for (int i = 1; i <= middle - from; ++i) {
-        if (middle + i >= s.length() || s[middle - 1] != toCompare || s[middle - i] != s[middle + i]) {
-            return false;
+long count_middle(const std::string &s, const int &middle) {
+    long counter = 0;
+    char ch;
+    for (int i = 1; middle - i >= 0 && middle + i < s.length(); ++i) {
+        if (i == 1) {
+            ch = s[middle - i];
+        }
+        if (ch == s[middle - i] && ch == s[middle + i]) {
+            counter++;
+        } else {
+            return counter;
         }
     }
-    return true;
+    return counter;
 }
 
 long substrCount(int n, std::string s) {
@@ -28,39 +34,30 @@ long substrCount(int n, std::string s) {
     int groupFirst = 0;
     for (int i = 1; i < s.length(); ++i) {
         if (s[i] != s[groupFirst]) {
-            int elements = i - groupFirst;
-            while (!is_middle(s, groupFirst, i) && groupFirst != i) {
-                groupFirst++;
-            }
-            if (is_middle(s, groupFirst, i)) {
-                std::cout << s.substr(groupFirst, (i - groupFirst) * 2 + 1) << " ";
-                counter += 1 + 2 * combinations(i - groupFirst);
-                groupFirst = i;
-            } else {
-                if (elements > 1) {
-                    counter += combinations(elements);
-                    groupFirst = i - 1;
-                } else {
-                    groupFirst = i;
-                }
-            }
-            i = groupFirst;
+            counter += combinations(i - groupFirst);
+            groupFirst = i;
         } else if (i == s.length() - 1) {
-            counter += combinations(s.length() - groupFirst);
+            counter += combinations(i - groupFirst + 1);
         }
     }
+
+    for (int i = 1; i < s.length(); ++i) {
+        if (s[i] != s[i - 1]) {
+            counter += count_middle(s, i);
+        }
+    }
+
     return counter + s.size();
 }
 
-
 int main() {
 //    std::cout << is_middle("aassa", 0, 2);
-//    std::cout << substrCount(std::string("mnonopoo").size(), "mnonopoo") << std::endl; //12
-//    std::cout << substrCount(std::string("asasd").size(), "asasd") << std::endl; //7
-//    std::cout << substrCount(std::string("abcbaba").size(), "abcbaba") << std::endl; //10
-//    std::cout << substrCount(std::string("aaaa").size(), "aaaa") << std::endl; //10
+    std::cout << substrCount(std::string("mnonopoo").size(), "mnonopoo") << std::endl; //12
+    std::cout << substrCount(std::string("asasd").size(), "asasd") << std::endl; //7
+    std::cout << substrCount(std::string("abcbaba").size(), "abcbaba") << std::endl; //10
+    std::cout << substrCount(std::string("aaaa").size(), "aaaa") << std::endl; //10
     std::cout << substrCount(std::string("aaaa").size(), "cccacc") << std::endl; //12
-//    std::cout << substrCount(std::string("aaaa").size(),
-//                             global::file_to_string("/code/Source/22_special_string/test.txt")) << std::endl; //1272919
+    std::cout << substrCount(std::string("aaaa").size(),
+                             global::file_to_string("/code/Source/22_special_string/test.txt")) << std::endl; //1272919
 }
 
