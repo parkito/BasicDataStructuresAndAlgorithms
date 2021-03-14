0fdd44af6b46daa3e5aced48270f649b4bc11b70
parkito <artyom-karnov@yandex.ru>
2021-03-13 20:50:56 -0700

    Improve special string

--- c/Source/22_special_string/SpecialString.cpp
+++ c/Source/22_special_string/SpecialString.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
+#include <map>
 
-bool isSpecial(const std::string &st, const int &from, const int &to) {
-    char first = st[from];
-    int subStringSize = to - from;
-    int mid = -1;
-    if (subStringSize > 1 && (subStringSize + 1) % 2 == 1) {
-        mid = from + subStringSize / 2;
+long combinations(const int num) {
+    long com = 0;
+    for (int i = 1; i <= num - 1; ++i) {
+        com += i;
     }
-    for (int i = from + 1; i <= to; ++i) {
-        if (st[i] != first) {
-            if (mid > 0) {
-                if (i != mid) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
+    return com;
+}
+
+bool is_middle(const std::string &s, const int &from, const int &middle) {
+    for (int i = 1; i < middle - from; ++i) {
+        if (middle + i >= s.length() || s[middle - i] != s[middle + i]) {
+            return false;
         }
     }
     return true;
@@ -26,20 +23,30 @@ long substrCount(int n, std::string s) {
         return n;
     }
     long counter = 0;
-    for (int i = 1; i < n; ++i) {
-        for (int j = 0; j + i < n; ++j) {
-            if (isSpecial(s, j, j + i)) {
-                counter++;
+    int groupFirst = 0;
+    for (int i = 1; i < s.length(); ++i) {
+        if (s[i] != s[groupFirst]) {
+            int elements = i - groupFirst;
+            if (is_middle(s, groupFirst, i)) {
+                counter += 1 + 2 * combinations(elements);
+                groupFirst = i + elements;
+            } else {
+                counter += combinations(elements);
+                groupFirst = i;
             }
+            i = groupFirst;
+        } else if (i == s.length() - 1) {
+            counter += combinations(s.length() - groupFirst);
         }
     }
-    return counter + n;
+    return counter + s.size();
 }
+
 
 int main() {
     std::cout << substrCount(std::string("mnonopoo").size(), "mnonopoo") << std::endl; //12
     std::cout << substrCount(std::string("asasd").size(), "asasd") << std::endl; //7
     std::cout << substrCount(std::string("abcbaba").size(), "abcbaba") << std::endl; //10
     std::cout << substrCount(std::string("aaaa").size(), "aaaa") << std::endl; //10
-};
+}
 
