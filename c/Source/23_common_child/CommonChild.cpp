c9ac1505c15ffd06bfe951363036dbe935bd3aca
parkito <artyom-karnov@yandex.ru>
2021-03-15 22:14:11 -0600

    Implement obvious common child

--- c/Source/23_common_child/CommonChild.cpp
+++ c/Source/23_common_child/CommonChild.cpp
@@ -35,28 +35,24 @@ bool contains(std::string &str, std::vector<char> &sub, int &from) {
 
 int commonChild(std::string s1, std::string s2) {
     int matrix[s1.length() + 1][s2.length() + 1];
-    for (int i = 0; i <= s1.length(); ++i) {
-        matrix[i][0] = 0;
-    }
-    for (int i = 0; i <= s2.length(); ++i) {
-        matrix[0][i] = 0;
-    }
-    for (int i = 1; i <= s1.length(); ++i) {
-        for (int j = 1; j <= s2.length(); ++j) {
-            if (s1[i - 1] == s2[j - 1]) {
-                matrix[i][j] = matrix[i - j][j - 1] + 1;
-            } else {
-                matrix[i][j] = std::max(matrix[i][j - 1], matrix[i - 1][j]);
-            }
-        }
+    for (int i = 0; i <= s1.length(); i++) {
+        for (int j = 0; j <= s2.length(); j++) {
+            if (i == 0 || j == 0)
+                matrix[i][j] = 0;
 
+            else if (s1[i - 1] == s2[j - 1])
+                matrix[i][j] = matrix[i - 1][j - 1] + 1;
+
+            else
+                matrix[i][j] = std::max(matrix[i - 1][j], matrix[i][j - 1]);
+        }
     }
-    int a= matrix[s1.length()][s1.length()];
-    return a;
+
+    return matrix[s1.length()][s2.length()];
 }
 
 int main() {
     std::cout << commonChild("HARRY", "SALLY") << std::endl;//2
     std::cout << commonChild("SHINCHAN", "NOHARAAA") << std::endl;//3
-    std::cout << commonChild("AA", "BB") << std::endl;//3
+    std::cout << commonChild("AA", "BB") << std::endl;//0
 }
