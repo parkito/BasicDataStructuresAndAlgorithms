2da37830cd14e93cff35c5033390d6833f10123d
parkito <artyom-karnov@yandex.ru>
2021-03-16 19:28:39 -0600

    Implement obvious common child

--- c/Source/23_common_child/CommonChild.cpp
+++ c/Source/23_common_child/CommonChild.cpp
@@ -2,49 +2,17 @@
 #include <unordered_set>
 #include <vector>
 
-std::unordered_set<char> to_set(std::string &str) {
-    std::unordered_set<char> set = std::unordered_set<char>();
-    for (auto &ch:str) {
-        set.insert(ch);
-    }
-    return set;
-}
-
-std::vector<char> to_substr(std::string &str, std::unordered_set<char> set) {
-    std::vector<char> substr = std::vector<char>();
-    for (auto &ch:str) {
-        if (set.find(ch) != set.end()) {
-            substr.push_back(ch);
-        }
-    }
-    return substr;
-}
-
-bool contains(std::string &str, std::vector<char> &sub, int &from) {
-    for (int i = 0, j = from; i < str.length(); ++i) {
-        if (str[i] == sub[j] && i != j) {
-            j++;
-        }
-        if (j == sub.size() - 1) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 int commonChild(std::string s1, std::string s2) {
     int matrix[s1.length() + 1][s2.length() + 1];
     for (int i = 0; i <= s1.length(); i++) {
         for (int j = 0; j <= s2.length(); j++) {
-            if (i == 0 || j == 0)
+            if (i == 0 || j == 0) {
                 matrix[i][j] = 0;
-
-            else if (s1[i - 1] == s2[j - 1])
+            } else if (s1[i - 1] == s2[j - 1]) {
                 matrix[i][j] = matrix[i - 1][j - 1] + 1;
-
-            else
+            } else {
                 matrix[i][j] = std::max(matrix[i - 1][j], matrix[i][j - 1]);
+            }
         }
     }
 
