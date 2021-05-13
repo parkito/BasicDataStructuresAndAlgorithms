fbdc14b60c7183e2c8f03e0254d2ca7ee2a53c9b
parkito <artyom-karnov@yandex.ru>
2021-05-13 00:18:34 -0400

    Add bag implementation

--- c/Ds/Bag/Bag.cpp
+++ c/Ds/Bag/Bag.cpp
@@ -1,4 +1,55 @@
-//
-// Created by artyom.karnov on 5/12/21.
-//
+#include "Bag.h"
+
+template<typename T>
+Bag<T>::Bag() {
+    items = 0;
+    allocated = REP_CONST;
+    store = new T[REP_CONST];
+}
+
+template<typename T>
+Bag<T>::~Bag() {
+    delete store;
+}
+
+template<typename T>
+void Bag<T>::add(T &item) {
+    if (items >= allocated) {
+        size_t newAllocated = allocated * REP_INC_CONST;
+        T *newStore = new T[newAllocated];
+        extend_store(0, allocated, store, newStore);
+        allocated = newAllocated;
+        delete store;
+        store = newStore;
+    }
+
+    store[items++] = item;
+}
+
+template<typename T>
+void Bag<T>::extend_store(std::size_t from, std::size_t to, T *arrFrom, T *arrTo) {
+    for (std::size_t i = from; i < to; ++i) {
+        arrTo[i] = arrFrom[i];
+    }
+}
+
+template<typename T>
+bool Bag<T>::isEmpty() {
+    return items == 0;
+}
+
+template<typename T>
+std::size_t Bag<T>::size() {
+    return items;
+}
+
+template<typename T>
+BagIterator<T> Bag<T>::begin() {
+    return BagIterator<T>(&store[0]);
+}
+
+template<typename T>
+BagIterator<T> Bag<T>::end() {
+    return BagIterator<T>(&store[allocated + 1]);
+}
 
--- c/Ds/Bag/Bag.h
+++ c/Ds/Bag/Bag.h
@@ -6,9 +6,11 @@
 template<typename T>
 class Bag {
 public:
+    Bag();
+
     ~Bag();
 
-    void add(T item);
+    void add(T &item);
 
     bool isEmpty();
 
@@ -17,4 +19,14 @@ public:
     BagIterator<T> begin();
 
     BagIterator<T> end();
+
+private:
+    T *store;
+    std::size_t items;
+    std::size_t allocated;
+
+    static const std::size_t REP_CONST = 10;
+    static const std::size_t REP_INC_CONST = 2;
+
+    void extend_store(std::size_t from, std::size_t to, T *arrFrom, T *arrTo);
 };
